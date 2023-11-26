eef7f76d735332bc68b47af4ee24263e3fc83378
kokoro-hart <tobita@wd-flat.com>
2023-11-26 17:55:57 +0900

    Data fetch in server component

---
+++ app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import { RouterButton } from "../components/router-button"
+
+export default function BlogPage() {
+  return (
+    <div className="m-10 text-center">
+      <span className="text-lg">Click a title on the left to view detail 🚀</span>
+      <div className="my-5 flex justify-center">
+        <RouterButton />
+      </div>
+    </div>
+  )
+}
